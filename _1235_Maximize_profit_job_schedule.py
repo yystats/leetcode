623e7590ee1c1a54ac2d0c0e158edb7575e0eba7
Chengwei "David" Yuan <yystats@gmail.com>
2025-01-25 16:18:54 -0800

    lc 1235

--- _1235_Maximize_profit_job_schedule.py
+++ _1235_Maximize_profit_job_schedule.py
@@ -19,11 +19,50 @@ Time range [1-3]+[3-6] , we get profit of 120 = 50 + 70.
 '''
 Thinking process:
 
-This is by nature a DFS problem. Each job is like a Node, including start time, end time, and profit. 
-Using the top-down approach, we can first sort the job by starting time, and then at any Node i, 
+This is by nature a DFS problem. Each job is like a Node, including start time, end time, and profit.
 
+Using the top-down approach, we can first sort the job by starting time, and then at any Node i, the max profit only have two scenarios:
+1) scenario 1 - include this Node, so does the proift; and find the next Node (j) that start time >= current Node end time 
+2) scenario 2 - does not include this Node, and basically we start with next Node (i+1)
+
+So the max profit is just max(scenarion1, scenario2)
+
+For scenario 1 - finding next Node is a binary search as the start time is an increasing list. We can use bisect_left to find the index. 
+
+In addition, DFS can be enhanced with memorization, so we can store the value in a dictionary for speeding up. 
 
 '''
+
+from bisect import bisect_left
+
+def jobSchedule(startTime, endTime, profit):
+    # sort by start time
+    jobs = list(zip(startTime, endTime, profit))
+    jobs.sort()
+    startTime.sort()
+    
+    n = len(jobs)
+    memo = {} # memorize the intermediate values in the dfs for speeding up 
+    
+    def dfs(i):
+        if i == n: return 0
+        if i in memo: return memo[i]
+        
+        s, e, p = jobs[i] # current job 
+
+        # we have two scenarios
+        # 1) include current ith profit + the following job that does not overlap with the current one  
+        # 2) does not include current ith profit, and start with the (i+1)th job
+        # 3) choose the max of the two scenarios 
+
+        j = bisect_left(startTime, e)
+        s1 = p + dfs(j)
+        s2 = dfs(i+1)
+        memo[i] = max(s1, s2)
+
+        return memo[i]
+
+    return dfs(0)
 
 
 
