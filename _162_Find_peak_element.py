c65b2b2046ff9a8c092ce820a2991d6a6470e95b
Chengwei "David" Yuan <yystats@gmail.com>
2025-01-27 21:50:13 -0800

    leetcode 162

---
+++ _162_Find_peak_element.py
@@ -0,0 +1,32 @@
+'''
+A peak element is an element that is strictly greater than its neighbors.
+
+Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+You must write an algorithm that runs in O(log n) time.
+
+Example:
+
+Input: nums = [1,2,1,3,5,6,4]
+Output: 5
+Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
+
+'''
+
+def findPeakElement(nums):
+    l, r = 0, len(nums) - 1
+    while l < r:
+        mid = (l + r) // 2
+        if nums[mid] < nums[mid + 1]:
+            l = mid + 1
+        else:
+            r = mid
+
+    return r
+
+
+nums = [1,2,1,3,5,6,4]
+print(findPeakElement(nums) == 5)
+
